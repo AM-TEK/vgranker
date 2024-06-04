fe09a5fef2e72fd8e48cd6c43dad953a72f4fb56
Alejandro Maldonado <am@AM-WEBDEV.attlocal.net>
2024-06-03 20:12:44 -0500

    breakout platform gradients into own file

---
+++ components/PlatformGradients.js
@@ -0,0 +1,18 @@
+const platformGradients = {
+  NES: 'bg-nes-custom-gradient',
+  Genesis: 'bg-genesis-custom-gradient',
+  Dreamcast: 'bg-dreamcast-custom-gradient',
+  SNES: 'bg-snes-custom-gradient',
+  N64: 'bg-n64-custom-gradient',
+  Gamecube: 'bg-gamecube-custom-gradient',
+  GameboyAdvance: 'bg-gba-custom-gradient',
+  PS2: 'bg-ps2-custom-gradient',
+  PS3: 'bg-ps3-custom-gradient',
+  Xbox: 'bg-xbox-custom-gradient',
+  Xbox360: 'bg-xbox360-custom-gradient',
+  WindowsPC: 'bg-windowspc-custom-gradient',
+  default: 'bg-gray-200',
+};
+
+export default platformGradients;
+
--- components/VideoGameCard.js
+++ components/VideoGameCard.js
@@ -1,35 +1,10 @@
+import platformGradients from './PlatformGradients';
 import { FaArrowUp } from 'react-icons/fa';
 
 const getPlatformGradientClass = (platform) => {
-  switch (platform) {
-    case 'NES':
-      return 'bg-nes-custom-gradient';
-    case 'Genesis':
-      return 'bg-genesis-custom-gradient';
-    case 'Dreamcast':
-      return 'bg-dreamcast-custom-gradient';
-    case 'SNES':
-      return 'bg-snes-custom-gradient';
-    case 'N64':
-      return 'bg-n64-custom-gradient';
-    case 'Gamecube':
-      return 'bg-gamecube-custom-gradient';
-    case 'GameboyAdvance':
-      return 'bg-gba-custom-gradient';
-    case 'PS2':
-      return 'bg-ps2-custom-gradient';
-    case 'PS3':
-      return 'bg-ps3-custom-gradient';
-    case 'Xbox':
-      return 'bg-xbox-custom-gradient';
-    case 'Xbox360':
-      return 'bg-xbox360-custom-gradient';
-    case 'WindowsPC':
-      return 'bg-windowspc-custom-gradient';
-    default:
-      return 'bg-gray-200';
-  }
-};
+  const gradientClass = platformGradients[platform] || platformGradients.default;
+  return gradientClass;
+};  
 
 const VideoGameCard = ({ videoGames = [], onClick, onMoveToTop }) => {
   if (!videoGames || videoGames.length === 0) {
@@ -38,22 +13,22 @@ const VideoGameCard = ({ videoGames = [], onClick, onMoveToTop }) => {
 
   return (
     <div className="container grid items-center justify-center gap-2 grid-cols">
-      {videoGames.map((videoGame) => (
+      {videoGames.map(({ id, title, developer, platform, year }) => (
         <div 
-          key={videoGame.id}
-          data-id={videoGame.id} 
-          onClick={() => onClick(videoGame)}
+          key={id}
+          data-id={id} 
+          onClick={() => onClick({ id, title, developer, platform, year })}
           draggable="true"
-          className={`relative flex flex-col min-h-[120px] min-w-[200px] w-4/5 px-2 text-black border border-gray-400 rounded-md cursor-move draggable justify-self-center ${getPlatformGradientClass(videoGame.platform)}`}>
-            <h2 className="font-bold text-md">{videoGame.title}</h2>
+          className={`relative flex flex-col min-h-[120px] min-w-[200px] w-4/5 px-2 text-black border border-gray-400 rounded-md cursor-move draggable justify-self-center ${getPlatformGradientClass(platform)}`}>
+            <h2 className="font-bold text-md">{title}</h2>
             <div className="absolute text-right bottom-2 right-2">
-              <p className="text-sm">{videoGame.developer}</p>
-              <p className="text-sm">{videoGame.platform}</p>
-              <p className="text-sm">{videoGame.year}</p>
+              <p className="text-sm">{developer}</p>
+              <p className="text-sm">{platform}</p>
+              <p className="text-sm">{year}</p>
             </div>
             <button
               onClick={(e) => {
-                e.stopPropagation(); // Prevent click on the button from triggering the card's click event
+                e.stopPropagation();
                 onMoveToTop(videoGame.id);
               }}
               className="absolute p-1 bg-gray-200 rounded-full bottom-2 left-2"
